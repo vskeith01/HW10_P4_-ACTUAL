904b98a9cde0bb35535fbacc1f265bf0e2b41c2d
vskeith <111769909+vskeith01@users.noreply.github.com>
2022-10-28 15:44:59 +0000

    COMPLETE CODE

---
+++ .ccls-cache/@home@runner@HW10P4-ACTUAL/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  const int NUMS = 5;
+
+  int nums[NUMS] = {16, 54, 7, 43, -5};
+  int total =0;
+  int *nPt;
+
+  nPt = nums;    // store address of nums[0] in nPt
+
+  for (int i = 0; i < NUMS; i++)
+    {
+      total += *nPt++;
+    }
+
+  cout << "The total of the array elements is " << total << endl;
+
+  return 0;
+  
+  
+  
+  }
